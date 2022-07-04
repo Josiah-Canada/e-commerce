e16f6d9e984e514e58ba0ed0077d93cf4ca98291
Josiah-Canada <josiahcanada@yahoo.com>
2022-07-04 16:43:05 -0400

    added table

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,23 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+CREATE TABLE category (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    category_name string NOT NULL
+);
+
+CREATE TABLE product (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    product_name string NOT NULL,
+    price DECIMAL NOT NULL,
+    stock INTEGER NOT NULL (10),
+    category_id INTEGER NOT NULL
+);
+
+CREATE TABLE ProductTag (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    Product_id INTEGER NOT NULL,
+    tag_id INTEGER NOT NULL
+);
+
+
--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const routes = require('./routes');
+const mysql = require('mysql2');
+const { ConnectionError } = require('sequelize/types');
 // import sequelize connection
+
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: 'Newsqlwhodis!97',
+    database: 'ecommerce_db'
+  },
+  console.log('Connected to e-commerce database')
+);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,3 +28,24 @@ app.use(routes);
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
+
+const Category ={
+  id:1,
+  name: "category_name",
+  init: function() {
+    connection.query(`CREATE TABLE CATEGORY(
+      id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+      name VARCHAR(10),
+    )`, function(error, data) {
+      // data is an array with table info
+    });
+  },
+  gather: function() {
+    connection.query(`SELECT * FROM Category
+    )`, function(error, data) {
+      // print everything
+    });
+  },
+
+
+}
